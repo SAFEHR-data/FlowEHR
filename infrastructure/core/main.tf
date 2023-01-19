a5e47b20b71b5b8e49f0bcb75da3fd1aee7c82f9
jjgriff93 <jamesgr@microsoft.com>
2023-01-19 17:41:29 +0000

    Add core module scaffold

---
+++ infrastructure/core/main.tf
@@ -0,0 +1,74 @@
+resource "azurerm_resource_group" "core" {
+  name     = "${var.prefix}-${var.environment}-rg-core"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "core" {
+  name                = "${var.prefix}-${var.environment}-vnet-core"
+  resource_group_name = azurerm_resource_group.core.name
+  location            = azurerm_resource_group.core.location
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_subnet" "core" {
+  name                 = "${var.prefix}-${var.environment}-subnet-core"
+  resource_group_name  = azurerm_resource_group.core.name
+  virtual_network_name = azurerm_virtual_network.core.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_storage_account" "core" {
+  name                     = "${var.prefix}${var.environment}strcore"
+  resource_group_name      = azurerm_resource_group.core.name
+  location                 = azurerm_resource_group.core.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.core.id]
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "${var.prefix}-${var.environment}-kv-core"
+  location                    = azurerm_resource_group.core.location
+  resource_group_name         = azurerm_resource_group.core.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
