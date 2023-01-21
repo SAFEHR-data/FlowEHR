7d3c3a94c34eab446f491adaace3c52d659b1c73
Tom Young <tom.young.316@gmail.com>
2023-01-21 14:39:23 +0000

    Use only precommit

--- infrastructure/core/outputs.tf
+++ infrastructure/core/outputs.tf
@@ -13,13 +13,13 @@
 #  limitations under the License.
 
 output "core_rg_name" {
-    value = azurerm_resource_group.core.name
+  value = azurerm_resource_group.core.name
 }
 
 output "core_rg_location" {
-    value = azurerm_resource_group.core.location
+  value = azurerm_resource_group.core.location
 }
 
 output "core_kv_id" {
-    value = azurerm_key_vault.core.id
+  value = azurerm_key_vault.core.id
 }
