27e6c1672af5e03bd3fc53d53b29c1f5948adae6
Tanya Borisova <tborisova@microsoft.com>
2023-01-27 19:50:25 +0000

    Clarify some comments

--- transform/pipeline/entrypoint.py
+++ transform/pipeline/entrypoint.py
@@ -11,5 +11,7 @@ spark = SparkSession(sc)
 if __name__ == "__main__":
     df = spark.createDataFrame([(1, ), (2, ), (3, ), (2, ), (3, )],
                                ["value"])
+    # This is an example of how transform from a built Python wheel library
+    # will be used in the entrypoint pipeline
     out_df = example_transform(df)
     out_df.display()
