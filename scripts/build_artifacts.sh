cb1864666e35ff4e09e5f91a709a1579a795258e
Tanya Borisova <tborisova@microsoft.com>
2023-02-15 12:04:33 +0000

    Check out pipeline repos

---
+++ scripts/build_artifacts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#  Copyright (c) University College London Hospitals NHS Foundation Trust
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
+
+
+ROOT=/workspaces/FlowEHR
+shopt -s globstar nullglob
+
+# Walk through files in /transform/pipelines
+# For each directory where activities.json has been found
+for activity_json_file in "${ROOT}"/transform/pipelines/**/activities.json; do 
+    pipeline_dir=$(dirname "${activity_json_file}")
+    pushd "${pipeline_dir}"
+    make artifacts
+    popd
+done
---
+++ scripts/pipeline_repo_checkout.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
+
+ROOT=/workspaces/FlowEHR
+
+# shellcheck disable=SC1090
+source "${ROOT}/scripts/load_env.sh"
+
+
+pushd "${ROOT}/transform/pipelines" > /dev/null
+export IFS=";"
+for repo in $REPOSITORIES; do
+  # Name for the directory to check out to,
+  # e.g. git@github.com:UCLH-Foundry/Data-Pipeline.git becomes Data-Pipeline 
+  dir_name=$(basename "${repo}" | sed -e 's/\.git$//')
+
+  # If the directory with checked out repo already exists, remove it
+  if [ -d "${dir_name}" ]; then
+    rm -rf "${dir_name}"
+  fi
+
+  git clone "${repo}"
+done
+
+popd > /dev/null
