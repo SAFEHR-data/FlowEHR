99e02a359068696e770ad4629e23618a421c159d
jedheaj314 <51018779+jedheaj314@users.noreply.github.com>
2023-02-17 17:13:17 +0000

    Create creation of ADF data pipeline, activities and databricks job schedue (#85)
    * Rename ./transform/pipeline to ./transform/features
    
    * Add ADF pipeline trigger basic definition (json) file
    
    * Add json definition files for adf pipeline trigger, pipeline and linked service
    
    * Rename ADF components - json definition files
    
    * Remove linked service JSON definition
    
    * Add resource block for pipeline activity
    
    * Add terroform resource to upload small whl files
    
    * Add variables for whl file name and path to be used in terraform
    
    * Add building wheel file to deploy-transform command in the make file
    
    * Remove TODO command (redundant)
    
    * Remove redundant new lines
    
    * Change ADF databricks pipeline to run jobs after 15 instead of 5
    
    * Resolve review comments - add upload, use locals intf file
    
    * Clean code use separate file for local variables, get rid of white spaces
    
    * Update infrastructure/transform/main.tf
    
    Co-authored-by: James Griffin <me@JamesGriff.in>
    
    * Update infrastructure/transform/main.tf
    
    Co-authored-by: James Griffin <me@JamesGriff.in>
    
    * Update Makefile to call terraform infra in deploy command
    
    Co-authored-by: James Griffin <me@JamesGriff.in>
    
    * Update Makefile to rename command from terraform to infra
    
    Co-authored-by: James Griffin <me@JamesGriff.in>
    
    * Update infrastructure/transform/locals.tf
    
    Co-authored-by: James Griffin <me@JamesGriff.in>
    
    * Add space at the start of the main.tf file infra
    
    * Fix lint
    
    * Add dependency for databricks in adf pipeline creation
    
    * Update deploy command to build artifact for transform-infra (Fix deploy issue)
    
    ---------
    
    Co-authored-by: Ashitosh Jedhe (AJ) <ajedhe@microsoft.com>
    Co-authored-by: James Griffin <me@JamesGriff.in>

--- infrastructure/transform/locals.tf
+++ infrastructure/transform/locals.tf
@@ -1,3 +1,12 @@
 locals {
-  storage_account_name = "dbfs${var.truncated_naming_suffix}"
+  storage_account_name    = "dbfs${var.truncated_naming_suffix}"
+  adb_linked_service_name = "ADBLinkedServiceViaMSI"
+
+  python_file_local_path = "../../transform/features"
+  python_file_dbfs_path  = "dbfs:"
+  python_file_name       = "entrypoint.py"
+
+  whl_file_local_path = "../../transform/features/dist"
+  whl_file_dbfs_path  = "dbfs:"
+  whl_file_name       = "src-0.0.1-py3-none-any.whl"
 }
