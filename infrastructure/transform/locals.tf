73349a5cba4646c6d94ce3234fdf25025b11a9dc
David Moore <damoo@microsoft.com>
2023-02-20 13:06:50 +0000

    merge error fixed

--- infrastructure/transform/locals.tf
+++ infrastructure/transform/locals.tf
@@ -1,8 +1,7 @@
 locals {
-  storage_account_name               = "dbfs${var.truncated_naming_suffix}"
   sql_server_features_admin_username = "adminuser"
-  storage_account_name    = "dbfs${var.truncated_naming_suffix}"
-  adb_linked_service_name = "ADBLinkedServiceViaMSI"
+  storage_account_name               = "dbfs${var.truncated_naming_suffix}"
+  adb_linked_service_name            = "ADBLinkedServiceViaMSI"
 
   python_file_local_path = "../../transform/features"
   python_file_dbfs_path  = "dbfs:"
