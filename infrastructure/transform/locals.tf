00dffee68461bb2506f12a57f6e2133e8f2e6772
Tanya Borisova <tborisova@microsoft.com>
2023-02-21 11:43:48 +0000

    Fix merge conflicts

--- infrastructure/transform/locals.tf
+++ infrastructure/transform/locals.tf
@@ -13,14 +13,15 @@
 #  limitations under the License.
 
 locals {
-  activities_file = "activities.json"
-  artifacts_dir   = "artifacts"
+  activities_file         = "activities.json"
+  artifacts_dir           = "artifacts"
+  adb_linked_service_name = "ADBLinkedServiceViaMSI"
 
   all_activities_files = fileset(path.module, "../../transform/pipelines/**/${local.activities_file}")
 
   # Example value: [ "../../transform/pipelines/hello-world" ]
   pipeline_dirs = toset([
-    for activity_file in all_activities_files : dirname(activity_file)
+    for activity_file in local.all_activities_files : dirname(activity_file)
   ])
 
   # Example value: [ { "artifact_path" = "path/to/entrypoint.py", "pipeline" = "hello-world" } ]
