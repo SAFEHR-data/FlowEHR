548777053c37637ec4494ac2b1fdb2ca6b8a0746
Tanya Borisova <tborisova@microsoft.com>
2023-02-23 11:47:57 +0000

    Try to check out with token if have one

--- scripts/pipeline_repo_checkout.sh
+++ scripts/pipeline_repo_checkout.sh
@@ -24,16 +24,10 @@ PIPELINE_DIR="${SCRIPT_DIR}/../transform/pipelines"
 CONFIG_PATH="${SCRIPT_DIR}/../config.transform.yaml"
 ORG_GH_TOKEN="${ORG_GH_TOKEN:-}" # May be unset
 
-if [[ -n "${TF_IN_AUTOMATION:-}" ]]; then
-  if [[ -n "${ORG_GH_TOKEN}" ]]; then
-    echo "${ORG_GH_TOKEN}" | gh auth login --with-token
-    REPO_CHECKOUT_COMMAND="gh repo clone"
-  else
-    echo "ORG_GH_TOKEN secret must be set in order to check out the repositories"
-    exit 1
-  fi
+if [[ -n "${ORG_GH_TOKEN}" ]]; then
+  echo "${ORG_GH_TOKEN}" | gh auth login --with-token
+  REPO_CHECKOUT_COMMAND="gh repo clone"
 else
-  # Using `git clone` for local flow instead of `gh` to avoid interactive prompt of `gh auth login`
   REPO_CHECKOUT_COMMAND="git clone"
 fi
 
