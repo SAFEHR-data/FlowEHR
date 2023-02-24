1a99ebf66258d524932719f9a2d4245e2375d825
jjgriff93 <jamesgr@microsoft.com>
2023-02-24 15:28:33 +0000

    ;Authentication=ActiveDirectoryMsi

--- apps/app/locals.tf
+++ apps/app/locals.tf
@@ -13,5 +13,5 @@
 #  limitations under the License.
 
 locals {
-  feature_store_odbc = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:${data.azurerm_mssql_server.feature_store.fully_qualified_domain_name},1433;Database=${var.feature_store_db_name};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+  feature_store_odbc = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:${data.azurerm_mssql_server.feature_store.fully_qualified_domain_name},1433;Database=${var.feature_store_db_name};Authentication=ActiveDirectoryMsi;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 }
