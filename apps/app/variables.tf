8a71d8124e1bbee0a8256da695464071f2f39e0d
James Griffin <me@JamesGriff.in>
2023-03-24 16:47:12 +0000

    Increase app_id length

--- apps/app/variables.tf
+++ apps/app/variables.tf
@@ -16,8 +16,8 @@ variable "app_id" {
   type = string
 
   validation {
-    condition     = length(var.app_id) <= 20
-    error_message = "app_id must be 20 chars or less"
+    condition     = length(var.app_id) <= 25
+    error_message = "app_id must be 25 chars or less"
   }
 
   validation {
