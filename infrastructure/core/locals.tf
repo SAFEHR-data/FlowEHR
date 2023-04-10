0d80022ddb767c9e436a724156766f42c7b7a6bb
James Griffin <me@JamesGriff.in>
2023-04-10 11:41:36 +0000

    Larger subnets

--- infrastructure/core/locals.tf
+++ infrastructure/core/locals.tf
@@ -16,9 +16,9 @@ locals {
   naming_suffix           = var.suffix_override == "" ? "${var.flowehr_id}-${var.environment}" : var.suffix_override
   naming_suffix_truncated = substr(replace(replace(local.naming_suffix, "-", ""), "_", ""), 0, 17)
 
-  # Split the /24 address space into (30, 30, 62, 62) usable IP addresses
+  # Split the /24 (or larger) address space into subnets of required sizes
   # for the different terraform modules/required delegations
-  subnet_address_spaces              = cidrsubnets(azurerm_virtual_network.core.address_space[0], 3, 3, 2, 2)
+  subnet_address_spaces              = cidrsubnets(azurerm_virtual_network.core.address_space[0], 2, 2, 2, 2)
   core_shared_address_space          = local.subnet_address_spaces[0]
   databricks_host_address_space      = local.subnet_address_spaces[1]
   databricks_container_address_space = local.subnet_address_spaces[2]
