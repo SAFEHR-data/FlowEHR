d49cc2311837fa97f9e5bd244c9ba3e66f9b8ec2
Tom Young <tom.young.316@gmail.com>
2023-04-14 14:21:09 +0000

    Add address space for aml subnet

--- infrastructure/core/locals.tf
+++ infrastructure/core/locals.tf
@@ -17,12 +17,26 @@ locals {
   naming_suffix_truncated = substr(replace(replace(local.naming_suffix, "-", ""), "_", ""), 0, 17)
 
   # Split the /24 (or larger) address space into subnets of required sizes
-  # for the different terraform modules/required delegations
-  subnet_address_spaces              = cidrsubnets(azurerm_virtual_network.core.address_space[0], 2, 2, 2, 2)
+  # for the different terraform modules/required delegations. Databricks requires > /27
+  address_space_is_slash24 = length(regexall("\\/24", azurerm_virtual_network.core.address_space[0])) > 0
+  databricks_newbits       = local.address_space_is_slash24 ? 2 : 3
+  other_newbits            = local.address_space_is_slash24 ? 4 : 3
+
+  subnet_address_spaces = cidrsubnets(
+    azurerm_virtual_network.core.address_space[0],
+    3,
+    local.databricks_newbits,
+    local.databricks_newbits,
+    3,
+    local.other_newbits,
+    local.other_newbits
+  )
   core_shared_address_space          = local.subnet_address_spaces[0]
   databricks_host_address_space      = local.subnet_address_spaces[1]
   databricks_container_address_space = local.subnet_address_spaces[2]
   serve_webapps_address_space        = local.subnet_address_spaces[3]
+  aml_address_space                  = local.subnet_address_spaces[4]
+  # free_address_space               = local.subnet_address_spaces[5]
 
   required_private_dns_zones = {
     blob       = "privatelink.blob.core.windows.net"
