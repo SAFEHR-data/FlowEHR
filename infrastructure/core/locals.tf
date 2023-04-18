dfffd571320604f98818e5939f70c25c76fa1093
David Moore <damoo@microsoft.com>
2023-04-18 19:04:59 +0000

    added postgres to required dns zones

--- infrastructure/core/locals.tf
+++ infrastructure/core/locals.tf
@@ -31,5 +31,6 @@ locals {
     cosmos     = "privatelink.documents.azure.com"
     databricks = "privatelink.azuredatabricks.net"
     sql        = "privatelink.database.windows.net"
+    postgres   = "privatelink.postgres.database.azure.com"
   }
 }
