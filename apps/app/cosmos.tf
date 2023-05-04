ad608ff1cab05bf96cc5e42a0833e150468ebf03
jedheaj314 <jedheaj3@gmail.com>
2023-05-04 08:30:58 +0000

    Remove optional properties of cosmos container

--- apps/app/cosmos.tf
+++ apps/app/cosmos.tf
@@ -36,31 +36,9 @@ resource "azurerm_cosmosdb_sql_role_assignment" "cosmos_access" {
 }
 
 resource "azurerm_cosmosdb_sql_container" "example" {
-  name                  = "${var.app_id}-container"
-  resource_group_name   = var.resource_group_name
-  account_name          = var.cosmos_account_name
-  database_name         = azurerm_cosmosdb_sql_database.app.name
-  partition_key_path    = "/definition/id"
-  partition_key_version = 1
-  throughput            = 400
-
-  indexing_policy {
-    indexing_mode = "consistent"
-
-    included_path {
-      path = "/*"
-    }
-
-    included_path {
-      path = "/included/?"
-    }
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-  }
-
-  unique_key {
-    paths = ["/definition/idlong", "/definition/idshort"]
-  }
+  name                = var.app_id
+  resource_group_name = var.resource_group_name
+  account_name        = var.cosmos_account_name
+  database_name       = azurerm_cosmosdb_sql_database.app.name
+  partition_key_path  = "/id"
 }
